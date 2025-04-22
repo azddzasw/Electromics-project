77005666f9ee1b76f153c5a40f937f52232d485e
azddza <azddza@tianhuangyuci.local>
2025-04-22 05:29:14 +0200

    ANI

---
+++ scripts/run_fastANI.sh
@@ -0,0 +1,8 @@
+./external/FastANI/build/fastANI \
+  --ql data/genome_list.txt \
+  --rl data/genome_list.txt \
+  --fragLen 1000 \
+  --minFraction 0.1 \
+  -t 8 \
+  -o results/fastani_output_relaxed.txt
+
