7c033d71609a427896f71f54756f0480c1c7126c
azddza <azddza@tianhuangyuci.local>
2025-04-24 22:17:45 +0200

    transfor

--- scripts/run_fastANI.sh
+++ scripts/run_fastANI.sh
@@ -2,7 +2,8 @@
   --ql data/genome_list.txt \
   --rl data/genome_list.txt \
   --fragLen 1000 \
-  --minFraction 0.1 \
+  --minFraction 0.15 \
   -t 8 \
-  -o results/fastani_output_relaxed.txt
+  -o results/fastani_output_1000_015.txt
 
+ 
